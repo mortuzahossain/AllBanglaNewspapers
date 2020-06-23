4b2a4810b35e15aa9e8c661c5e0368e4b503772f
Mortuza Hossain <mortuzahossain1997@gmail.com>
2020-06-23 11:39:35 +0600

    Details Screen Done.

--- app/src/main/java/io/github/mortuzahossain/allbanglanewspapers/views/DetailsActivity.java
+++ app/src/main/java/io/github/mortuzahossain/allbanglanewspapers/views/DetailsActivity.java
@@ -1,16 +1,65 @@
 package io.github.mortuzahossain.allbanglanewspapers.views;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.github.mortuzahossain.allbanglanewspapers.R;
 
 public class DetailsActivity extends AppCompatActivity {
 
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
+    @BindView(R.id.webView)
+    WebView webView;
+    @BindView(R.id.swipeRefresh)
+    SwipeRefreshLayout swipeRefresh;
+
+    private static String LOAD_URL = "LOAD_URL";
+
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        ButterKnife.bind(this);
+
+        String URL = getIntent().getStringExtra(LOAD_URL);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(URL);
+        swipeRefresh.setOnRefreshListener(() -> webView.loadUrl(webView.getUrl()));
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                swipeRefresh.setRefreshing(false);
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int newProgress) {
+                progressBar.setProgress(newProgress);
+                if (newProgress == 100) {
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+
     }
 }
